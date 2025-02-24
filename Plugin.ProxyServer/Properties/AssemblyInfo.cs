a2b427c93b53d493ebbedec1466e9703d92a46fd
DKorablin2 <danila.korablin@unity3d.com>
2025-02-24 03:38:24 +0300

    Unify plugins deployment

--- Plugin.ProxyServer/Properties/AssemblyInfo.cs
+++ Plugin.ProxyServer/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("0529e909-b42f-43c9-80a6-91d8d054a84a")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,15 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://github.com/DKorablin/Plugin.ProxyServer")]
 #else
 
-[assembly: AssemblyTitle("Plugin.ProxyServer")]
 [assembly: AssemblyDescription("HTTP(S) proxy server plugin based on Titanium.Web.Proxy")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.ProxyServer")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2018-2024")]
 #endif
 
